cd533c41f3891407d3ad471c173b10eb694b64e6
unknown <sanvicola2000@gmail.com>
2021-02-19 14:14:38 +0100

    Added Navbar, implemented Dark mode toggle functionality, Search Box and Filter Box

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <header>
+  <header :class="{ light: !reactive.darkMode }">
     <h6>Where in the world?</h6>
-    <div class="theme-switcher">
-      <h6>Dark Mode</h6>
-    </div>
+    <button class="theme-switcher" @click="toggleDarkMode">
+      <i class="bx bx-moon" :class="{ 'bxs-moon': reactive.darkMode }"></i>
+      {{ reactive.darkMode ? "Dark Mode" : "Light Mode" }}
+    </button>
   </header>
-  <router-view/>
+  <router-view />
 </template>
+
+
+<script>
+export default {
+  provide() {
+    return {
+      reactive: this.reactive,
+    };
+  },
+  data() {
+    return {
+      reactive: {
+        darkMode: true,
+      },
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.reactive.darkMode = !this.reactive.darkMode;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;500;600;700&display=swap");
@@ -24,9 +48,52 @@ body {
   font-family: "Nunito Sans", sans-serif;
 }
 
-#app {
-  color: #2c3e50;
-  // background-color: #35495e;
-}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: hsl(209, 23%, 22%);
+  padding: 25px 60px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  border-bottom: 1px solid rgba(0, 0, 0, 0);
+  transition: all 0.5s ease-in-out;
 
+  h6 {
+    color: #fff;
+    font-size: 18px;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    outline: none;
+    border: none;
+    background: none;
+    font: inherit;
+    font-weight: 600;
+    font-size: 16px;
+    cursor: pointer;
+
+    i {
+      font-size: 15px;
+      margin-top: -2px;
+      margin-right: 5px;
+    }
+  }
+
+  &.light {
+    background: hsl(0, 0%, 100%);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.09);
+
+    button,
+    h6 {
+      color: hsl(200, 15%, 8%);
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 25px 20px;
+  }
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,165 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
+  <div class="home" :class="{ light: !reactive.darkMode }">
+    <div class="input-and-filter">
+      <form @submit.prevent="search">
+        <div class="input-field">
+          <input
+            type="search"
+            v-model="country"
+            placeholder="Search for a Country"
+          />
+          <i class="bx bx-search search-icon"></i>
+        </div>
+      </form>
+      <div class="filter-box">
+        <select name="" id="">
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceanic">Oceanic</option>
+        </select>
+        <i class="bx bxs-chevron-down"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-}
+  inject: ["reactive"],
+  name: "Home",
+  data() {
+    return {
+      country: "",
+    };
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background: hsl(207, 26%, 17%);
+  padding: 30px 60px;
+  transition: all 0.5s ease-in-out;
+
+  .input-and-filter {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    form {
+      width: 400px;
+
+      .input-field {
+        position: relative;
+
+        input {
+          width: 100%;
+          height: 50px;
+          border: none;
+          border-radius: 5px;
+          background-color: hsl(209, 23%, 22%);
+          color: hsl(0, 0%, 100%);
+          font: inherit;
+          padding: 0 20px 0 60px;
+          box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+
+          &::placeholder {
+            color: hsl(0, 0%, 100%);
+          }
+        }
+
+        .search-icon {
+          position: absolute;
+          left: 20px;
+          top: 14px;
+          color: hsl(0, 0%, 100%);
+          font-size: 22px;
+        }
+      }
+
+      @media screen and (max-width: 650px) {
+        width: 100%;
+        margin-bottom: 30px;
+      }
+    }
+
+    .filter-box {
+      width: 100%;
+      max-width: 200px;
+      position: relative;
+
+      select {
+        width: 100%;
+        appearance: none;
+        border: none;
+        outline: none;
+        padding: 0 12px;
+        height: 50px;
+        border-radius: 5px;
+        background-color: hsl(209, 23%, 22%);
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+        color: hsl(0, 0%, 100%);
+        font: inherit;
+        cursor: pointer;
+
+        option {
+          font: inherit;
+        }
+      }
+
+      i {
+        pointer-events: none;
+        position: absolute;
+        right: 10px;
+        top: 16px;
+        color: hsl(0, 0%, 100%);
+      }
+    }
+
+    @media screen and (max-width: 650px) {
+      align-items: flex-start;
+      flex-direction: column;
+    }
+  }
+
+  &.light {
+    background: hsl(0, 0%, 98%);
+
+    .input-and-filter {
+      form {
+        .input-field {
+          input {
+            color: hsl(200, 15%, 8%);
+            background: hsl(0, 0%, 98%);
+
+            &::placeholder {
+              color: hsl(200, 15%, 8%);
+            }
+          }
+
+          .search-icon {
+            color: hsl(200, 15%, 8%);
+          }
+        }
+      }
+
+      .filter-box {
+        select {
+          color: hsl(200, 15%, 8%);
+          background: hsl(0, 0%, 98%);
+        }
+
+        i {
+          color: hsl(200, 15%, 8%);
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 30px 20px;
+  }
+}
+</style>
