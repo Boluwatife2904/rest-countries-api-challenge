578c53215b72e3fbf8fd7a8968e996b3031953bc
unknown <sanvicola2000@gmail.com>
2021-02-19 12:02:30 +0100

    Removed unnecessary files and setup basic things

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header>
+    <h6>Where in the world?</h6>
+    <div class="theme-switcher">
+      <h6>Dark Mode</h6>
+    </div>
+  </header>
   <router-view/>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;500;600;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: "Nunito Sans", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  // background-color: #35495e;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
