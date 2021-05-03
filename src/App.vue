647ecac6991c397c42b14cf385ee1dae2efc8c54
unknown <sanvicola2000@gmail.com>
2021-05-03 19:45:30 +0100

    Moved Header from App.vue into its own component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <header :class="{ light: !darkMode }">
-   <router-link :to="{ name: 'Home' }">Where in the world?</router-link>
-    <button class="theme-switcher" @click="toggle">
-      <i class="bx bx-moon" :class="{ 'bxs-moon': darkMode }"></i>
-      {{ darkMode ? "Dark Mode" : "Light Mode" }}
-    </button>
-  </header>
+  <Navbar />
   <router-view />
 </template>
 
 
 <script>
-import { mapGetters } from "vuex";
+import Navbar from "./components/Navbar.vue"
 
 export default {
-  computed: {
-    ...mapGetters(["darkMode"]),
-  },
-  methods: {
-    toggle() {
-      this.$store.commit("toggle")
-    },
-  },
+  components: { Navbar },
 };
 </script>
 
@@ -39,55 +26,5 @@ html {
 
 body {
   font-family: "Nunito Sans", sans-serif;
-}
-
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: hsl(209, 23%, 22%);
-  padding: 25px 60px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  border-bottom: 1px solid rgba(0, 0, 0, 0);
-  transition: all 0.3s;
-
-  a {
-    color: #fff;
-    font-size: 18px;
-    text-decoration: none;
-  }
-
-  button {
-    display: flex;
-    align-items: center;
-    color: #fff;
-    outline: none;
-    border: none;
-    background: none;
-    font: inherit;
-    font-weight: 600;
-    font-size: 16px;
-    cursor: pointer;
-
-    i {
-      font-size: 15px;
-      margin-top: -2px;
-      margin-right: 5px;
-    }
-  }
-
-  &.light {
-    background: hsl(0, 0%, 100%);
-    border-bottom: 1px solid rgba(0, 0, 0, 0.09);
-
-    button,
-    a {
-      color: hsl(200, 15%, 8%);
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    padding: 25px 20px;
-  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <header :class="{ light: !darkMode }">
+   <router-link :to="{ name: 'Home' }">Where in the world?</router-link>
+    <button class="theme-switcher" @click="toggle">
+      <i class="bx bx-moon" :class="{ 'bxs-moon': darkMode }"></i>
+      {{ darkMode ? "Dark Mode" : "Light Mode" }}
+    </button>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Navbar",
+  computed: {
+    ...mapGetters(["darkMode"]),
+  },
+  methods: {
+    toggle() {
+      this.$store.commit("toggle")
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: hsl(209, 23%, 22%);
+  padding: 25px 60px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  border-bottom: 1px solid rgba(0, 0, 0, 0);
+  transition: all 0.3s;
+
+  a {
+    color: #fff;
+    font-size: 18px;
+    text-decoration: none;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    outline: none;
+    border: none;
+    background: none;
+    font: inherit;
+    font-weight: 600;
+    font-size: 16px;
+    cursor: pointer;
+
+    i {
+      font-size: 15px;
+      margin-top: -2px;
+      margin-right: 5px;
+    }
+  }
+
+  &.light {
+    background: hsl(0, 0%, 100%);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.09);
+
+    button,
+    a {
+      color: hsl(200, 15%, 8%);
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 25px 20px;
+  }
+}
+</style>
